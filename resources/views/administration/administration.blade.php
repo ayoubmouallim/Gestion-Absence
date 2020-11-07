e6ea992c21788b09379d9369507807b42932a0f1
yassine mabrouk <mabroukyassine70@gmail.com>
2020-11-07 02:09:11 +0100

    add administration space

--- resources/views/administration/administration.blade.php
+++ resources/views/administration/administration.blade.php
@@ -1,23 +1,43 @@
-@extends('layouts.app')
+@extends('layouts.administration')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard Administration</div>
+<section class="about text-center">
+    <div class="container">
+    <h1>Espace <span>Administration</span> </h1>
+    <p class="lead">Bienvenue dans votre espace vous pouvez utiliser cette application <strong> Gestion des absences </strong> pour voir la liste des étudiants, voir le détail des absences de chaque étudiant et envoyer des avertissements aux étudiants qui ont beaucoup des absences
+   </div>
+  </section>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  <section class="features text-center">
+   <div class="container">
+     <h1>Servises</h1>
+     <div class="row"><!-- featues1 -->
+       <div class=" col-md-4  col-xs-12 ">
+         <div class="feat"> <!--my div -->
+           <i class="fa fa-list-alt" style="font-size:48px;" aria-hidden="true"></i>
+           <h4>Liste des étudiants </h4>
+          <p class="lead">Visualiser la liste des etudiants  dans tous les filières </p>
+          <a href="#" class="btn btn-primary">Start</a>
+         </div>
+       </div>
+                  <div class=" col-md-4  col-xs-12 ">
+         <div class="feat"> <!--my div -->
+           <i class="fa fa-user" style="font-size:48px;" aria-hidden="true"></i>
+          <h4> Détail des absences des étudiants </h4>
+          <p class="lead">Visualiser le détail des absences de chaque étudiants </p>
+          <a href="#" class="btn btn-primary">Start</a>
+         </div>
+       </div>
+         
+       <div class=" col-md-4  col-xs-12 ">
+         <div class="feat"> <!--my div -->
+           <i class="fa fa-pencil"  style="font-size:48px;" aria-hidden="true"></i>
+          <h4>Envoyer des avertissemets</h4>
+          <p class="lead"> Envoyer des avertissements aux étudiants qui ont beaucoup des absences</p>
+          <a href="#" class="btn btn-primary">Start</a>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
 @endsection
